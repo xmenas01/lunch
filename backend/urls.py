75aae9b36b43db654ce97665acc074380e97cc17
Vytautas Kapturauskas <vyka@danskebank.lt>
2022-01-24 17:28:56 +0200

    Adding docker-compose

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from lunch import views
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'restaurant', views.RestaurantViewSet, basename="restaurant")
@@ -9,4 +11,4 @@ router.register(r'restaurant', views.RestaurantViewSet, basename="restaurant")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
